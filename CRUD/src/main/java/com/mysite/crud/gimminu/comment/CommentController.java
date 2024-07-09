58a2356cacd18ff58047a741abd583e5f98fbcc1
min-yeonghyeon <yoon61765@gmail.com>
2024-07-10 05:28:50 +0900

    Reapply "Merge remote-tracking branch 'origin/main'"
    This reverts commit 9c43d3b5fb76792cc173977bf2d10b762fb9ba69.

---
+++ CRUD/src/main/java/com/mysite/crud/gimminu/comment/CommentController.java
@@ -0,0 +1,18 @@
+package com.mysite.crud.gimminu.comment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/gimminu/comment")
+@RequiredArgsConstructor
+@Controller
+public class CommentController {
+
+    private final CommentService commentService;
+    @PostMapping("/create/{noticeId}")
+    public String createComment(@PathVariable("noticeId") Integer noticeId, @RequestParam("content") String content) {
+        commentService.createComment(noticeId,content);
+        return String.format("redirect:/gimminu/notice/notice_detail/%s", noticeId);
+    }
+}
