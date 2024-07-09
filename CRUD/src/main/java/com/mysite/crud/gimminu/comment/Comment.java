289145cfdfa261eb43bc1b820da710b2ac136526
min-yeonghyeon <yoon61765@gmail.com>
2024-07-10 05:18:40 +0900

    Reapply "Merge remote-tracking branch 'origin/main'"
    This reverts commit 9c43d3b5fb76792cc173977bf2d10b762fb9ba69.

---
+++ CRUD/src/main/java/com/mysite/crud/gimminu/comment/Comment.java
@@ -0,0 +1,22 @@
+package com.mysite.crud.gimminu.comment;
+
+import com.mysite.crud.gimminu.notice.Notice;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime date;
+    @ManyToOne
+    private Notice notice;
+}
