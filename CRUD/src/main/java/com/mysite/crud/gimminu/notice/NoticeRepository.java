9c43d3b5fb76792cc173977bf2d10b762fb9ba69
min-yeonghyeon <yoon61765@gmail.com>
2024-07-10 05:14:39 +0900

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 8e3761dd6f49981afecd7cdaaaa4fa9618caf4fc, reversing
    changes made to 22fd6753055d7033640943615c38117335b28e80.

--- CRUD/src/main/java/com/mysite/crud/gimminu/notice/NoticeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mysite.crud.gimminu.notice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<Notice, Integer> {
-}
