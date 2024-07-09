d3e6ad4bc65b5d75e5de78e88f3c2e5e7c83b592
Gimminu <jingjing1254@naver.com>
2024-07-09 22:43:35 +0900

    CRUD complete

---
+++ CRUD/src/main/java/com/mysite/crud/gimminu/notice/NoticeService.java
@@ -0,0 +1,36 @@
+package com.mysite.crud.gimminu.notice;
+
+import com.mysite.crud.gimminu.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service@RequiredArgsConstructor
+public class NoticeService {
+    private final NoticeRepository noticeRepository;
+
+    public void create(Notice notice){
+        notice.setDate(LocalDateTime.now());
+        noticeRepository.save(notice);
+    }
+    public List<Notice> getList(){
+        return noticeRepository.findAll();
+    }
+    public Notice getNotice(int id) {
+        Optional<Notice> notice = noticeRepository.findById(id);
+        if(notice.isPresent()){
+            return notice.get();
+        }else {
+            throw new DataNotFoundException("Notice not found");
+        }
+    }
+    public void deleteById(Integer id){
+        noticeRepository.deleteById(id);
+    }
+    public void update(Notice notice){
+        noticeRepository.save(notice);
+    }
+}
