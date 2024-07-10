6782a43c7a4a72cd1726f42764e0121dc353b76c
Gimminu <jingjing1254@naver.com>
2024-07-10 21:32:47 +0900

    gimminu 파트 수정
    파일 수정 및 추가

--- CRUD-name tag/src/main/java/com/mysite/crud/gimminu/notice/NoticeController.java
+++ CRUD-name tag/src/main/java/com/mysite/crud/gimminu/notice/NoticeController.java
@@ -1,10 +1,13 @@
 package com.mysite.crud.gimminu.notice;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RequestMapping("/gimminu/notice")
@@ -12,32 +15,38 @@ import java.util.List;
 @Controller("gimminuNoticeController")
 public class NoticeController {
     private final NoticeService noticeService;
-
+    @Value("${cloud.aws.s3.endpoint}")
+    private String downpath;
     @GetMapping("/notice_board")
     public String noticeBoard(Model model) {
         List<Notice> noticeList =this.noticeService.getList();
         model.addAttribute("notice_list", noticeList);
         return "/gimminu/notice/notice_board";
     }
+
     @GetMapping("/notice_create")
     public String noticeCreate() {
         return "/gimminu/notice/notice_create";
     }
     @PostMapping("/notice_create")
-    public String noticeCreate(@ModelAttribute Notice notice) {
-        this.noticeService.create(notice);
+    public String noticeCreate(@ModelAttribute Notice notice,
+                               @RequestParam("files") List<MultipartFile> files) {
+        try {
+            noticeService.create(notice, files);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "redirect:/gimminu/notice/notice_board";
+        }
         return "redirect:/gimminu/notice/notice_board";
-    }
-    @GetMapping("notice_detail")
-    public String noticedetail(){
-        return "/gimminu/notice/notice_detail";
     }
     @GetMapping("/notice_detail/{id}")
     public String noticeDetail(@PathVariable int id, Model model) {
         Notice notice =this.noticeService.getNotice(id);
         model.addAttribute("notice",notice);
+        model.addAttribute("downpath","https://"+downpath);
         return "gimminu/notice/notice_detail";
     }
+
     @GetMapping("/notice_update/{id}")
     public String noticeUpdate(@PathVariable int id, Model model) {
         model.addAttribute("notice",noticeService.getNotice(id));
@@ -48,10 +57,10 @@ public class NoticeController {
         noticeService.update(notice);
         return "redirect:/gimminu/notice/notice_detail/" + notice.getId();  // 올바른 리디렉션 경로
     }
+
     @GetMapping("/notice_delete/{id}")
     public String noticeDelete(@PathVariable int id) {
         noticeService.deleteById(id);
         return "redirect:/gimminu/notice/notice_board";
     }
-
 }
--- CRUD-name tag/src/main/java/com/mysite/crud/gimminu/notice/NoticeService.java
+++ CRUD-name tag/src/main/java/com/mysite/crud/gimminu/notice/NoticeService.java
@@ -1,10 +1,15 @@
 package com.mysite.crud.gimminu.notice;
 
+import com.mysite.crud.S3Service;
 import com.mysite.crud.gimminu.DataNotFoundException;
+import com.mysite.crud.gimminu.Image.Image;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,8 +17,29 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class NoticeService {
     private final NoticeRepository noticeRepository;
+    private final S3Service s3Service;
 
-    public void create(Notice notice){
+
+    public void create(Notice notice, List<MultipartFile> files) throws IOException {
+        if (files!= null && !files.isEmpty()) {
+            List<Image> images = new ArrayList<>();
+            for (MultipartFile file : files) {
+                if (!file.isEmpty()) {
+                    String fileKey = s3Service.uploadmanyFiles(file);
+                    Image image = new Image();
+                    image.setFilekey(fileKey);
+                    image.setNotice(notice);
+                    images.add(image);
+                }
+            }
+            if (!images.isEmpty()) {
+                if(notice.getImages() == null){
+                    notice.setImages(images);
+                }else {
+                    notice.getImages().addAll(images);
+                }
+            }
+        }
         notice.setDate(LocalDateTime.now());
         noticeRepository.save(notice);
     }
